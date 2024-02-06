fa5b2967e039aba3e96466b06525cd92cb41564d
Vicente Garcia <vicentegarciaayllon@gmail.com>
2024-02-06 19:47:01 +0100

    finish hero skeleton

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Layout from "@/components/layout";
 import { useContext, useEffect, useState } from "react";
 import { isPlayingContext } from "../context/context";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -26,14 +27,55 @@ export default function Home() {
     return (
       <Layout>
         <main className={styles.main}>
-          <h1 className={styles.jimena}>GONZALEZ<br></br>JIMENA</h1>
+          <h1 className={`${styles.jimena} ${styles.jimena_surname}`}>GONZALEZ</h1>
+          <h1 className={`${styles.jimena} ${styles.jimena_name}`}>JIMENA</h1>
           <section className={styles.hero}>
             <div className={styles.hero_top}>
               <div className={styles.links}>
-                <Link href={"https://www.linkedin.com/in/jimena-gonzalez-collados/"}>LINKEDIN →</Link>
+                <Link
+                  href={"https://www.linkedin.com/in/jimena-gonzalez-collados/"}
+                >
+                  LINKEDIN →
+                </Link>
+                <Link
+                  href={"https://www.linkedin.com/in/jimena-gonzalez-collados/"}
+                >
+                  STUDIO →
+                </Link>
+                <Link
+                  href={"https://www.linkedin.com/in/jimena-gonzalez-collados/"}
+                >
+                  BLOG →
+                </Link>
               </div>
+              <p>(STRATEGIC MIND / SENSITIVE HEART)</p>
+              <Link
+                href={"mailto:hi@jimenagonzalez.in"}
+                className={styles.contacto}
+              >
+                CONTÁCTAME
+              </Link>
             </div>
-            <div className={styles.hero_bottom}></div>
+            <div className={styles.hero_bottom}>
+              <p className={styles.scroll}>
+                SCROLL<br></br>↓
+              </p>
+              <p className={styles.text}>
+                Creo en el poder de la estrategia para crear negocios con
+                propósito, y en el del diseño para elevar el potencial de las
+                sinergias creativas.
+              </p>
+            </div>
+            <Image
+              src="/jimena_business.png"
+              width={0}
+              height={0}
+              sizes="50vw"
+              style={{ width: "54vw", height: "auto" }}
+              alt={"Service Designer Jimena picture of herself"}
+            ></Image>
+          </section>
+          <section className={styles.metodologia}>
           </section>
         </main>
       </Layout>
